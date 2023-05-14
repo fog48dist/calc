68ff56498c06dbd4d96c25531c5c4a6efb20c8be
fog48dist <113779310+fog48dist@users.noreply.github.com>
2023-05-14 14:02:37 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,31 @@
+
+var repoPath = '/calc';
+var assets = [    
+  `${repoPath}/`,
+  `${repoPath}/index.html`,
+  `${repoPath}/math.js`,
+  `${repoPath}/gear-svgrepo-com.svg`,
+]
+
+const cacheName = calc_cache_001
+
+self.addEventListener("install", installEvent => {
+  installEvent.waitUntil(
+    caches.open(cacheName).then(cache => {
+      return cache.addAll(assets)
+    })
+  )
+})
+
+  self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        caches.match(event.request)
+            .then(function(response) {
+                    if (response) {
+                        return response;
+                    }
+                    return fetch(event.request);
+                }
+            )
+    );
+});
