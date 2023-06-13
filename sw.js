487700ae784f3944082160524e1a0c8f66a109f9
fog48dist <vladnodster@protonmail.com>
2023-06-13 22:37:53 +0300

    minor changes

--- sw.js
+++ sw.js
@@ -7,7 +7,7 @@ var assets = [
   `${repoPath}/gear-svgrepo-com.svg`,
 ]
 
-const cacheName ="calc_cache_005"
+const cacheName ="calc_cache_006"
 
 self.addEventListener("install", installEvent => {
   installEvent.waitUntil(
